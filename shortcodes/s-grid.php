b278e11b9b18d62524a89f8a40ec963ad888fe82
Madars Bitenieks <madza@madzathemes.com>
2018-10-24 16:16:53 +0300

    fix

--- shortcodes/s-grid.php
+++ shortcodes/s-grid.php
@@ -327,7 +327,7 @@ function grid( $atts, $content = null ) {
 		              if ( has_post_thumbnail() ) {
 										if($i==1) {
 											$shortcode .='<div class="mt-slide-1-img">';
-												$shortcode .='<div class="mt-slide-1-image lazy" data-src="'. get_the_post_thumbnail_url(get_the_ID(),'magazin_1300').'"></div><div class="mt-post-image-background" style="background-image:url('. get_the_post_thumbnail_url(get_the_ID(),'magazin_1300').');"></div>';
+												$shortcode .='<div class="mt-slide-1-image lazy" data-src="'. get_the_post_thumbnail_url(get_the_ID(),'magazin_1300').'"></div>';
 											$shortcode .='</div>';
 										}
 									}
